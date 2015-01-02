8e45783dba20620130137e02f0ac41b42e76bd54
Andrew G. Chvyl <achvyl@gmail.com>
2015-01-02 16:57:39 +0200

    Update ActiveRecordUploadableImageBehavior.php
    make method saveImageFile() as static

--- ActiveRecordUploadableImageBehavior.php
+++ ActiveRecordUploadableImageBehavior.php
@@ -83,7 +83,7 @@ class ActiveRecordUploadableImageBehavior extends CActiveRecordBehavior {
      * @param  array $config Пропорции (width, height, minHeight, mimWidth, maxHeight, maxWidth)
      * @return image        Сохраненное изображение
      */
-    protected function saveImageFile($tempname, $filename, $imageSize = null) {
+    protected static function saveImageFile($tempname, $filename, $imageSize = null) {
         $imageFile = Yii::app()->image->load($tempname);
         $sizeProportion = $imageFile->width / $imageFile->height;
 
